294612e6953da08423505130108f314640414a40
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-12-03 08:56:04 -0500

    Added ControllerSetupScene.

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ Warp.Room = require('./server/game/rooms/Room.js');
 Warp.MultiplayerRoom = require('./server/game/rooms/MultiplayerRoom.js');
 Warp.Scene = require('./server/game/scenes/Scene.js');
 Warp.WaitForConsumerScene = require('./server/game/scenes/WaitForConsumerScene.js');
+Warp.ControllerSetupScene = require('./server/game/scenes/controller/ControllerSetupScene.js');
 
 module.exports = Warp;
---
+++ server/game/scenes/controller/ControllerSetupScene.js
@@ -0,0 +1,39 @@
+var setupScene = require('../Scene.js');
+var Controller = require('./Controller.js');
+
+var ControllerSetupScene = function(player, ...event_types) {
+  var controller = new Controller(player);
+
+  function requestControllerEvent(type) {
+    player.sendEventToConsumers({
+      'event_type': 'need_controller_event',
+      'type': type
+    });
+  };
+
+  function setupController(resolve) {
+    var index = 0;
+
+    function callback() {
+      if(index < event_types.length) {
+        var type = event_types[index];
+        index += 1;
+
+        requestControllerEvent(type);
+        controller.setNextEvent(type, callback);
+
+      } else {
+        controller.activate();
+        resolve(controller);
+      }
+    };
+
+    setImmediate(callback);
+  };
+
+  return setupScene('controller_setup', player).then(function() {
+    return new Promise(setupController);
+  });
+};
+
+module.exports = ControllerSetupScene;
