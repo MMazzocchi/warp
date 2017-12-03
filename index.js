26191d78db9a4b9fc44123e8434aa5dafc160f5b
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-12-03 11:03:18 -0500

    Removed MultiplayerRoom.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var Warp = {};
 
 Warp.Game = require('./server/game/Game.js');
 Warp.Room = require('./server/game/rooms/Room.js');
-Warp.MultiplayerRoom = require('./server/game/rooms/MultiplayerRoom.js');
 Warp.Scene = require('./server/game/scenes/Scene.js');
 Warp.WaitForConsumerScene = require('./server/game/scenes/WaitForConsumerScene.js');
 Warp.ControllerSetupScene = require('./server/game/scenes/controller/ControllerSetupScene.js');
