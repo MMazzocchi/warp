0fceefb99688bfb5370dd451235ab44b70f6823d
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-03-08 20:05:26 -0500

    Moved build process to gulp.

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+const CLIENT_SRC = './client/src/';
+const CLIENT_DIST = './client/dist/';
+
+function buildFile(in_file, out_dir, new_filename) {
+  var filename = new_filename;
+
+  if(filename === undefined) {
+    var tokens = in_file.split(/\/+/);
+    filename = tokens[tokens.length - 1];
+  }
+
+  return new Promise(function(resolve) {
+    var stream = browserify(in_file)
+      .bundle()
+      .pipe(source(filename))
+      .pipe(gulp.dest(out_dir));
+
+    stream.on('end', function() {
+      resolve(out_dir+"/"+filename);
+    });
+  });
+};
+
+function build() {
+  return Promise.all([
+    buildFile(CLIENT_SRC+'/WarpClient.js', CLIENT_DIST),
+  ]);
+};
+
+gulp.task('build', build);
+gulp.task('default', gulp.series('build'));
