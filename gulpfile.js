937a6b2e29a0ec3947f3853518e3df77edbdba35
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-03-18 16:38:22 -0400

    Added derequire to gulp bundle process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var derequire = require('gulp-derequire');
 
 const CLIENT_SRC = './client/src/';
 const CLIENT_DIST = './client/dist/';
@@ -17,6 +18,7 @@ function buildFile(in_file, out_dir, new_filename) {
     var stream = browserify(in_file)
       .bundle()
       .pipe(source(filename))
+      .pipe(derequire())
       .pipe(gulp.dest(out_dir));
 
     stream.on('end', function() {
