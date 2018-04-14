8775de1fd388fa15f91ae91f6665af672be89bf9
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-04-14 15:55:26 -0400

    Changed Menu to an event emitter.

--- server/Menu.js
+++ server/Menu.js
@@ -1,30 +1,28 @@
-var Menu = function(name, options, player) {
-  var that = {};
+var EventEmitter = require('events');
+
+var Menu = function(options) {
+  var that = new EventEmitter();
 
   // Fields
   var selection = 0;
-  var menu_title = name + "_menu";
 
   // Private methods
-  function sendSelectionData() {
-    var data = {
-      'event_type': menu_title,
-      'selection': menu.getSelectionIndex()
-    };
-
-    player.sendEventToConsumers(data);
+  function emit() {
+    that.emit('selection', options[selection]);
   };
 
   // Public methods
   that.next = function(value) {
     if((value === undefined) || (value === 1)) {
       selection = (selection + 1) % options.length;
+      emit();
     }
   };
 
   that.prev = function(value) {
     if((value === undefined) || (value === 1)) {
       selection = (selection + (options.length - 1)) % options.length;
+      emit();
     }
   };
 
